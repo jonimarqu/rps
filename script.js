96769878d575e05a642af309e19374f7c2c0f425
jonimarqu <marquez.jonas91@gmail.com>
2022-10-03 19:14:31 -0300

    Add first solution attemp, succeded.

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log(`Hello World!`);
+// Ask the user for a choice input
+
+let userChoice = (prompt(`Rock, Paper or Scissors?`)).toLowerCase();
+console.log(`You choose ${userChoice}.`);
+
+// Ask the computer for a choice input
+
+let random = Math.floor((Math.random()* 3 ) + 1);
+
+function getComputerChoice(random) {
+    if (random === 1) {
+        return `rock`;
+    } else if (random === 2) {
+        return `paper`;
+    } else {
+        return `scissors`;
+    }
+};
+
+let computerChoice = getComputerChoice(random);
+console.log(`Computer choose ${computerChoice}.`);
+
+
+function playRound(userChoice, computerChoice) {
+    if(userChoice === computerChoice) {
+      //  console.log(`It's a draw.`);
+        return `draw`;
+    } else if(userChoice === `rock` && computerChoice === `scissors`) {
+      //  console.log(`Player wins`);
+        return `player wins`;
+    } else if(userChoice === `paper` && computerChoice === `rock`) {
+      //  console.log(`Player wins`);
+        return `player wins`;
+    } else if(userChoice === `scissors` && computerChoice === `paper`) {
+      //  console.log(`Player wins`);
+        return `player wins`;
+    } else {
+      //  console.log(`Computer wins`);
+        return `computer wins`;
+    }
+};
+
+
+let gameOn = playRound(userChoice, computerChoice);
+console.log(gameOn);
