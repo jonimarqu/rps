d82b4444c01781f8395b22dad44f47fbfe1c293b
jonimarqu <marquez.jonas91@gmail.com>
2022-10-04 12:04:13 -0300

    Refursbish the hole code to make a 5 round game.

--- script.js
+++ script.js
@@ -1,45 +1,91 @@
 // Ask the user for a choice input
 
-let userChoice = (prompt(`Rock, Paper or Scissors?`)).toLowerCase();
-console.log(`You choose ${userChoice}.`);
+let userChoice;
+
+function getUserChoice() {
+  userChoice = prompt(`Rock, Paper or Scissors?`).toLowerCase();
+  console.log(`Player chose ${userChoice}.`)
+  return userChoice;
+}
 
 // Ask the computer for a choice input
 
-let random = Math.floor((Math.random()* 3 ) + 1);
+let random;
 
 function getComputerChoice(random) {
-    if (random === 1) {
-        return `rock`;
-    } else if (random === 2) {
-        return `paper`;
-    } else {
-        return `scissors`;
-    }
-};
+  if (random === 1) {
+    return `rock`;
+  } else if (random === 2) {
+    return `paper`;
+  } else {
+    return `scissors`;
+  }
+}
 
-let computerChoice = getComputerChoice(random);
-console.log(`Computer choose ${computerChoice}.`);
-
+let computerChoice;
 
 function playRound(userChoice, computerChoice) {
-    if(userChoice === computerChoice) {
-      //  console.log(`It's a draw.`);
-        return `draw`;
-    } else if(userChoice === `rock` && computerChoice === `scissors`) {
-      //  console.log(`Player wins`);
-        return `player wins`;
-    } else if(userChoice === `paper` && computerChoice === `rock`) {
-      //  console.log(`Player wins`);
-        return `player wins`;
-    } else if(userChoice === `scissors` && computerChoice === `paper`) {
-      //  console.log(`Player wins`);
-        return `player wins`;
-    } else {
-      //  console.log(`Computer wins`);
-        return `computer wins`;
-    }
+  
+  userChoice = getUserChoice();
+  random = Math.floor((Math.random()* 3 ) + 1);
+  computerChoice = getComputerChoice(random);
+  console.log(`Computer chose ${computerChoice}.`);
+
+  if(userChoice === computerChoice) {
+    console.log("Draw");
+    return `draw`;
+  } else if(userChoice === `rock` && computerChoice === `scissors`) {
+    console.log("Player wins");
+    return `player wins`;
+  } else if(userChoice === `paper` && computerChoice === `rock`) {
+    console.log("Player wins");
+    return `player wins`;
+  } else if(userChoice === `scissors` && computerChoice === `paper`) {
+    console.log("Player wins");
+    return `player wins`;
+  } else {
+    console.log("Computer wins");
+    return `computer wins`;
+  }
+
 };
 
+let playerCount = 0;
+let computerCount = 0;
 
-let gameOn = playRound(userChoice, computerChoice);
-console.log(gameOn);
+function game() {
+  for (let i = 0; i < 5; i++) {
+    switch (playRound(userChoice, computerChoice)) {
+      case `player wins`:
+        playerCount++;
+        console.log(playerCount);
+        break;
+      case `computer wins`:
+        computerCount++;
+        console.log(computerCount);
+        break;
+    }
+  }
+
+
+
+ /*   if (playRound(userChoice, computerChoice) === `player wins`) {
+      playerCount++;
+      console.log(`Player counter is ${playerCount}.`);
+    } else if (playRound(userChoice, computerChoice) === `computer wins`) {
+      computerCount++;
+      console.log(`Computer counter is ${computerCount}.`);
+    };
+  } */ 
+
+
+  if (playerCount > computerCount) {
+    return `Player wins!`;
+  } else if (playerCount < computerCount) {
+    return `Computer wins!`;
+  } else {
+    return `DRAW!`;
+  }
+}
+
+console.log(game());
